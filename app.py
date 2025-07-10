5b885a894521b3a5d6cd3cf8c4f8433707cfaa86
wenxuan26 <chuawenxuan532@gmail.com>
2025-07-10 14:29:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
+import streamlit as st
+import pandas as pd
+import base64
+import requests
 
+st.title("Read CSV from GitHub")
+
+url = "https://raw.githubusercontent.com/limfw/temp/main/data.csv"
+
+def load_data():
+  return pd.read_csv(url)
+
+try:
+  df = load_data()
+  st.success("Data loaded successfully!")
+  st.dataframe(df)
+except Exception as e:
+  st.error(f"Failed to load data: (e)")
